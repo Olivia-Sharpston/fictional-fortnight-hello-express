e43bcb242abbdbf643b934b5737fd948d6b91bf9
Sharpston <osharpston@una.edu>
2025-08-28 13:03:58 -0500

    got static pages working

--- app.mjs
+++ app.mjs
@@ -1,35 +1,25 @@
 import express from 'express'
-// commented out
-// import path from 'path';
-// import {dirname} from 'node:path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
 const app = express()
-// commented out
-// const path = require('path');
-// const app = express();
 const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// app.use(express.static(__dirname + 'public'));
-
-
+app.use(express.static(join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Hello Express from Render. <a href="/olivia">olivia</a>')
 })
 // endpoints...middlewares...apis?
+
 // send to an html file
-
 app.get('/olivia', (req, res) => {
-  //res.send('olivia. <a href="/">home</a>')
-
-  // commented out
-  // res.sendFile('olivia.html');
+  res.sendFile(join(__dirname, 'public', 'olivia.html')) 
 })
 
 
-
-// app.listen(3000)
-// TODO: refactor to use env port
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
 })
