c491fc07e81db87c1826303e1b9f6c903e9cea23
Sharpston <osharpston@una.edu>
2025-09-02 13:37:31 -0500

    issue 4 from class

--- app.mjs
+++ app.mjs
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 app.use(express.static(join(__dirname, 'public')));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello Express from Render. <a href="/olivia">olivia</a>')
@@ -23,6 +24,32 @@ app.get('/api/olivia', (req, res) => {
   // res.send('olivia. <a href="/">home</a>')
   const myVar = 'Hello from server!';
   res.json({ myVar });
+})
+
+app.get('/api/query', (req, res) => {
+  //console.log("client request with query param:", req.query.name);
+  const name = req.query.name;
+  res.json({"message": `Hi, ${name}. How are you?`});
+})
+
+app.get('/api/url/:id', (req, res) => {
+  console.log("client request with URL param:", req.params.id);
+  // const name = req.query.name;
+  // res.json({"message": `Hi, ${name}. How are you?`});
+})
+
+app.get('/api/body', (req, res) => {
+  console.log("client request with POST body:", req.query);
+  // console.log("client request with POST body:", req.body.name);
+  // const name = req.body.name;
+  // res.json({"message": `Hi, ${name}. How are you?`});
+})
+
+app.post('/api/body', (req, res) => {
+  // console.log("client request with POST body:", req.query);
+  console.log("client request with POST body:", req.body.name);
+  const name = req.body.name;
+  res.json({"message": `Hi, ${name}. How are you?`});
 })
 
 app.listen(PORT, () => {
