0e9ebf8000ec7106f072f57e455e669c9963da97
Sharpston <osharpston@una.edu>
2025-09-04 13:19:35 -0500

    iss7

--- app.mjs
+++ app.mjs
@@ -16,6 +16,30 @@ console.log("get .env uri:", uri);
 app.use(express.static(join(__dirname, 'public')));
 app.use(express.json());
 
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
   res.send('Hello Express from Render. <a href="/olivia">olivia</a>')
 })
